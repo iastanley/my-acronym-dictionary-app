417c1e7081b198bbd125e3abaa471c0c310ce2a2
iastanley <iasvks.craigslist@gmail.com>
2017-04-08 22:28:52 -0700

    finished first version of models.js for acronym category and user models

---
+++ models.js
@@ -0,0 +1,56 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
+
+//model for acronym entries
+const acronymSchema = mongoose.Schema({
+  userId: {type: String, required: true},
+  acronym: {type: String, required: true},
+  spellOut: {type: String, required: true},
+  definition: String,
+  categoryId: {type: String}
+});
+
+const Acronym = mongoose.model('Acronym', acronymSchema);
+
+//model for categories
+const categorySchema = mongoose.Schema({
+  title: String,
+  color: String
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+//model for users
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.methods.apiResponse = function() {
+  return {
+    username: this.username
+  }
+}
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+//statics is like a class method - can be called directly on Model
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {Acronym, Category, User};
