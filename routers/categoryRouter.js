b55e1be430f184d9579747d497adfd97aea6eaab
iastanley <iasvks.craigslist@gmail.com>
2017-04-10 20:04:10 -0700

    added POST route to Category router

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -36,3 +36,29 @@ router.get('/:id', (req, res) => {
       res.status(500).json({message: 'Internal server error'});
     });
 });
+
+router.post('/', (req, res) => {
+  const requiredFields = ['title', 'color'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      const message = `Missing '${field}' in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+
+  Category
+    .create({
+      title: req.body.title,
+      color: req.body.color
+    })
+    .then(category => {
+      res.status(201).json(category.apiResponse());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+module.exports = router;
